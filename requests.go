b86252b0416ae4c5abe267432822751c0b3af9e0
zyf <>
2019-05-23 18:55:02 +0800

    update requests.go and add comment

--- requests.go
+++ requests.go
@@ -11,23 +11,28 @@ import (
 	neturl "net/url"
 )
 
+// Params save key-value pairs which will be attached behind the url
+type Params map[string]interface{}
+
+// Response
 type response struct {
 	*http.Response
 	data []byte
 }
 
-type Params map[string]interface{}
-
+// return response string data
 func (resp *response) String() string { return string(resp.data) }
 
-func (resp *response) Reader() io.Reader {
-	return interface{}(bytes.NewReader(resp.data)).(io.Reader)
-}
+// return an io.Reader which save the response bytes data
+func (resp *response) Reader() io.Reader { return interface{}(bytes.NewReader(resp.data)).(io.Reader) }
 
+// return response bytes data
 func (resp *response) Bytes() []byte { return resp.data }
 
+// unmarshal response data into v
 func (resp *response) Unmarshal(v interface{}) error { return json.Unmarshal(resp.data, v) }
 
+// attach params behind url
 func genURL(url string, params map[string]interface{}) string {
 	var ps = neturl.Values{}
 	for k, v := range params {
@@ -40,6 +45,7 @@ func genURL(url string, params map[string]interface{}) string {
 	return url
 }
 
+// Send a Get Request
 func Get(url string, params map[string]interface{}) (*response, error) {
 	var (
 		r    = &response{}
@@ -59,6 +65,7 @@ func Get(url string, params map[string]interface{}) (*response, error) {
 	return r, nil
 }
 
+// Send a Post Request with form
 func Post(url string, params map[string]interface{}, f *form) (resp *response, err error) {
 	if f == nil {
 		resp, err = postNilForm(url, params)
@@ -73,21 +80,25 @@ func Post(url string, params map[string]interface{}, f *form) (resp *response, e
 	return
 }
 
-type form struct {
-	fields  map[string][]interface{}
-	hasFile bool
-}
-
+// file which will be sended
 type postfile struct {
 	name string
 	data []byte
 }
 
+// web form
+type form struct {
+	fields  map[string][]interface{}
+	hasFile bool
+}
+
+// add one field into form
 func (f *form) AddField(name string, value interface{}) *form {
 	f.fields[name] = append(f.fields[name], value)
 	return f
 }
 
+// add one file into form
 func (f *form) AddFile(name, filename string, data []byte) *form {
 	f.hasFile = true
 	f.fields[name] = append(f.fields[name], postfile{
@@ -97,6 +108,7 @@ func (f *form) AddFile(name, filename string, data []byte) *form {
 	return f
 }
 
+// generate a new form
 func NewForm() *form {
 	return &form{
 		make(map[string][]interface{}),
@@ -104,6 +116,7 @@ func NewForm() *form {
 	}
 }
 
+// send post requests with file(s)
 func postWithFile(url string, params map[string]interface{}, f *form) (*response, error) {
 	var (
 		r    = &response{}
@@ -111,39 +124,37 @@ func postWithFile(url string, params map[string]interface{}, f *form) (*response
 		err  error
 	)
 
-	//创建一个模拟的form中的一个选项,这个form项现在是空的
+	// create a simulation form
 	bodyBuf := &bytes.Buffer{}
 	bodyWriter := multipart.NewWriter(bodyBuf)
-	if f != nil {
-		for fieldname, values := range f.fields {
-			for _, value := range values {
-				switch v := value.(type) {
-				case postfile:
-					//关键的一步操作, 设置文件的上传参数叫image, 文件名是tmp.png,
-					//相当于现在还没选择文件, form项里选择文件的选项
-					fileWriter, err := bodyWriter.CreateFormFile(fieldname, v.name)
+	for fieldname, values := range f.fields {
+		for _, value := range values {
+			switch v := value.(type) {
+			// add file
+			case postfile:
+				// create a file-upload item
+				fileWriter, err := bodyWriter.CreateFormFile(fieldname, v.name)
 
-					//bodyWriter.WriteField()
-					if err != nil {
-						return nil, err
-					}
-					//iocopy 这里相当于选择了文件,将文件放到form中
-					_, err = io.Copy(fileWriter, bytes.NewReader(v.data))
-					if err != nil {
-						return nil, err
-					}
-				default:
-					bodyWriter.WriteField(fieldname, fmt.Sprint(v))
+				if err != nil {
+					return nil, err
 				}
+
+				// put file data into file-upload item
+				if _, err = io.Copy(fileWriter, bytes.NewReader(v.data)); err != nil {
+					return nil, err
+				}
+
+			// add field
+			default:
+				bodyWriter.WriteField(fieldname, fmt.Sprint(v))
 			}
 		}
 	}
 
 	contentType := bodyWriter.FormDataContentType()
-
 	bodyWriter.Close()
 
-	//发送post请求到服务端
+	// send post requests with data
 	if r.Response, err = http.Post(_url, contentType, bodyBuf); err != nil {
 		return nil, err
 	}
@@ -156,6 +167,7 @@ func postWithFile(url string, params map[string]interface{}, f *form) (*response
 	return r, nil
 }
 
+// send post requests with only fields, no files
 func postWithoutFile(url string, params map[string]interface{}, f *form) (*response, error) {
 	var (
 		r    = &response{}
@@ -164,19 +176,17 @@ func postWithoutFile(url string, params map[string]interface{}, f *form) (*respo
 		data = neturl.Values{}
 	)
 
-	if f != nil {
-		for field, values := range f.fields {
-			for _, value := range values {
-				if data.Get(field) == "" {
-					data.Set(field, fmt.Sprint(value))
-					continue
-				}
-				data.Add(field, fmt.Sprint(value))
+	for field, values := range f.fields {
+		for _, value := range values {
+			if data.Get(field) == "" {
+				data.Set(field, fmt.Sprint(value))
+				continue
 			}
+			data.Add(field, fmt.Sprint(value))
 		}
 	}
 
-	//发送post请求到服务端
+	// send post request with data
 	if r.Response, err = http.PostForm(_url, data); err != nil {
 		return nil, err
 	}
@@ -190,6 +200,7 @@ func postWithoutFile(url string, params map[string]interface{}, f *form) (*respo
 
 }
 
+// send post request with nothing
 func postNilForm(url string, params map[string]interface{}) (*response, error) {
 	var (
 		r    = &response{}
@@ -197,6 +208,7 @@ func postNilForm(url string, params map[string]interface{}) (*response, error) {
 		err  error
 	)
 
+	// send post request
 	if r.Response, err = http.PostForm(_url, nil); err != nil {
 		return nil, err
 	}
@@ -208,6 +220,7 @@ func postNilForm(url string, params map[string]interface{}) (*response, error) {
 	return r, nil
 }
 
+// send post request with binary data
 func PostBinary(url string, params map[string]interface{}, body io.Reader) (*response, error) {
 	var (
 		r    = &response{}
@@ -215,7 +228,7 @@ func PostBinary(url string, params map[string]interface{}, body io.Reader) (*res
 		err  error
 	)
 
-	//发送post请求到服务端
+	// send post request
 	if r.Response, err = http.Post(_url, "multipart/form-data", body); err != nil {
 		return nil, err
 	}
